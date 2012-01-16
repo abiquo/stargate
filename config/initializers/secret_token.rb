419a010aa889851a3a886fc04d6ef632edd7fd18
Marc Morata Fité <marc.morata@abiquo.com>
2012-01-16 16:22:36 +0100

    Creating the rails 3.1 struct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stargate::Application.config.secret_token = '3b00f81533e9ad8991a5c8147695059f40e288e25162963b314e4270ff84b963fd28f44fa73e1f3821501a4e07eb8c478d86c4308ed551f89958dc6902b8f60f'
